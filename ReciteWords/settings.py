7d74b116a813f18454e5665f6659937f088928d3
wuteng <1303685031@qq.com>
2020-03-08 10:01:13 +0800

    切换mysql

--- ReciteWords/settings.py
+++ ReciteWords/settings.py
@@ -76,18 +76,18 @@ WSGI_APPLICATION = 'ReciteWords.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'recitewords',
-    #     'USER': 'root',
-    #     'PASSWORD': 'root',
-    #     'HOST': '127.0.0.1',
-    #     'PORT': '3306'
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'recitewords',
+        'USER': 'root',
+        'PASSWORD': 'wuteng..',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
+    }
 }
 
 
